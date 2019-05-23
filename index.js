dc2f6f4cfd6acd2483c9e622301a47f2e1872282
pluswar <hanwo46@naver.com>
2019-05-24 01:27:12 +0900

    js modified

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-function sayHello(name){
-  console.log('Hello! ' + name);
+/*function sayHello(name, age){
+  //console.log(`Hello ${name} you are ${age} years old`)
+  return `Hello ${name} you are ${age} years old` ;
 }
 
-sayHello("Hyuck Jae!!");
-console.log("HI");
+const greetName = sayHello("Hyuck Jae!!", 15);
+*/
+const calculator = {
+  plus: function(a, b){
+    return a+b;
+  },
+  minus: function(a, b){
+    return a-b;
+  },
+  multiply: function(a, b){
+    return a*b;
+  },
+  divide: function(a, b){
+    return a/b;
+  }
+}
+
+
+const plus = calculator.plus(5, 5);
+const minus = calculator.minus(5, 5);
+const multi = calculator.multiply(5, 5);
+const divide = calculator.divide(5, 5);
+
+console.log(plus);
+console.log(minus);
+console.log(multi);
+console.log(divide);
